2919348c1cb6dd5ec7fb45a7c5ad43345d4309da
Thibaut Moia <thibaut.moia@gmail.com>
2020-09-16 20:24:07 +0200

    update exercises on methods

---
+++ methods/ex_1.rb
@@ -0,0 +1,8 @@
+def greeting(name)
+  a = "this is so you, #{name}"
+end
+
+puts greeting(345)
+puts greeting('thibaut')
+greeting('Gordo')
+
---
+++ methods/ex_3.rb
@@ -0,0 +1,6 @@
+def multiply(num_1, num_2)
+  num_2 * num_1
+end
+
+
+puts multiply(39, 98)
