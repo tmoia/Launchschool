cb1546ceb82b131d9fee90df4e16679980c15ed3
Thibaut Moia <thibaut.moia@gmail.com>
2020-09-16 20:29:33 +0200

    update on nested files for struture purpose

---
+++ intro_to_programming/methods/ex_3.rb
@@ -0,0 +1,5 @@
+def multiply(num_1, num_2)
+  num_2 * num_1
+end
+
+puts multiply(39, 98)
---
+++ intro_to_programming/methods/ex_5.rb
@@ -0,0 +1,8 @@
+1. def scream(words)
+      words = words + "!!!!"
+      puts words
+    end
+
+    scream("Yippeee")
+
+ 2. still returns nil
