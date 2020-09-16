bb51655ae689a50824fc932aa14c4a9c57f16f51
Thibaut Moia <thibaut.moia@gmail.com>
2020-09-16 20:30:51 +0200

    deleted redundant files

--- methods/ex_1.rb
+++
@@ -1,8 +0,0 @@
-def greeting(name)
-  a = "this is so you, #{name}"
-end
-
-puts greeting(345)
-puts greeting('thibaut')
-greeting('Gordo')
-
--- methods/ex_3.rb
+++
@@ -1,6 +0,0 @@
-def multiply(num_1, num_2)
-  num_2 * num_1
-end
-
-
-puts multiply(39, 98)
